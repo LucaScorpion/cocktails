2c95b1e8e1299cd50808b45e0ba441d244e52c6e
Luca Scalzotto <luca@scalzotto.nl>
2025-09-27 22:38:34 +0200

    Basic grid and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,12 @@ import { cocktails } from './cocktails.ts';
 export const App = () => {
   return (
     <>
-      <h1>Luca's Cocktails</h1>
-      <CocktailGrid cocktails={cocktails}/>
+      <section>
+        <h1>Luca's Cocktails</h1>
+      </section>
+      <section>
+        <CocktailGrid cocktails={cocktails}/>
+      </section>
     </>
   );
 };
--- src/components/CocktailCard.tsx
+++ src/components/CocktailCard.tsx
@@ -1,12 +1,13 @@
 import type { Cocktail } from '../model.ts';
 import { Link } from 'react-router';
+import styles from './CocktailCard.module.scss';
 
 export interface Props {
   cocktail: Cocktail;
 }
 
 export const CocktailCard = ({cocktail}: Props) => (
-  <Link to={`/cocktails/${cocktail.id}`}>
+  <Link to={`/cocktails/${cocktail.id}`} className={styles.card}>
     {cocktail.name}
   </Link>
 );
